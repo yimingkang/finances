4b7a8ad71f7f6d681663c2842bb43b99eb712102
yimingkang <yiming.kang@mail.utoronto.ca>
2016-08-10 16:57:05 -0700

    Add db cache

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -22,6 +22,13 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'db_cache',
+    }
+}
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', ]
